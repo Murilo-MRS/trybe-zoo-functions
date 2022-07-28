0baf4579648e77e250df42ad8f08564d5f24760b
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-28 20:47:03 -0300

    requisitos 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+const { species } = data;
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  return (data.species.filter((element) => element.id === ids));
+  return species.filter(({ id: compId }) => ids.find((id) => compId === id));
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
