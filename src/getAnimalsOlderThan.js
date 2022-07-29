651ecb051c8110f6d64d9c9e1df2b770446f668f
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-28 21:17:44 -0300

    requisitos 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const { residents } = species.find(({ name }) => name === animal);
+  return residents.every(({ age: elementAge }) => elementAge >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,7 @@
 const data = require('../data/zoo_data');
 
 const { species } = data;
+// spread para jogar mais valores como param de getSpeciesByIds
 function getSpeciesByIds(...ids) {
   // seu código aqui
   return species.filter(({ id: compId }) => ids.find((id) => compId === id));
