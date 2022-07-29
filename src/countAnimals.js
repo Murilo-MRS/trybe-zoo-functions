6bc582d46f53554ce9ac1ef98cc08999ae24a687
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-29 19:53:51 -0300

    requisitos 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 function countAnimals(animal) {
   // seu código aqui
-}
+  if (!animal) {
+    return species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+  }
+  const { specie, sex } = animal;
 
+  const sameSpecie = ({ name }) => name === specie;
+  const sameSex = ({ sex: specieSex }) => specieSex === sex;
+  if (specie && sex) {
+    return species.find(sameSpecie).residents.filter(sameSex).length;
+  }
+  if (specie) {
+    return species.find(sameSpecie).residents.length;
+  }
+}
+console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 module.exports = countAnimals;
