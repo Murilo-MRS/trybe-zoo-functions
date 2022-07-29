93c4f7c173ba15caa173107e74f9be0e7e2e5909
Murilo-MRS <murilosantana1993@gmail.com>
2022-07-28 21:50:05 -0300

    requisitos 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  return employees.find(({ firstName, lastName }) =>
+    firstName === employeeName || lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
