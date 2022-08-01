60f29d0eaca8a89962095750c8d415e591c236c8
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-01 20:16:36 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
+const { employees } = data;
+const { species } = data;
+
+function getSpeciesByIds(id) {
   // seu código aqui
+  return species.find((element) => element.id === id);
 }
 
+function getOldestFromFirstSpecies(id) {
+  // seu código aqui
+  const firstSpecie = employees.find((e) => e.id === id).responsibleFor[0];
+  const { residents } = getSpeciesByIds(firstSpecie);
+  const { name, sex, age } = residents.reduce((acc, curr) => (acc.age < curr.age ? curr : acc));
+  return [name, sex, age];
+}
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
