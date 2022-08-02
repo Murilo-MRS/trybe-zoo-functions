e1ea195afd92dfd9e565237590b3ba2a7b456e53
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-02 16:26:08 -0300

    requisito 14 todos passaram

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,6 +2,24 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
+const arrayNomes = (animal, options) => {
+  const arrayResidentes = species.find((elemento) => elemento.name === animal).residents;
+
+  if (options.sex !== undefined && options.sorted === true) {
+    return arrayResidentes.filter((e) => e.sex === options.sex).map((elem) => elem.name).sort();
+  }
+  if (options.sex !== undefined) {
+    return arrayResidentes.filter((e) => e.sex === options.sex).map((element) => element.name);
+  }
+  if (options.sorted === true) {
+    return arrayResidentes.map((elem) => elem.name).sort();
+  }
+  return arrayResidentes.map((elem) => elem.name);
+};
+// console.log(arrayNomes('lions', { sex: 'male' }));
+// console.log(arrayNomes('lions', { sorted: true }));
+// console.log(arrayNomes('lions', { sex: 'male', sorted: true }));
+// console.log(arrayNomes('lions', { includeNames: true }));
 const animalRegionList = () => species.reduce((acc, curr) => {
   if (!acc[curr.location]) {
     acc[curr.location] = [];
@@ -11,26 +29,26 @@ const animalRegionList = () => species.reduce((acc, curr) => {
 }, {});
 
 // console.log(animalRegionList());
-const animalListWithName = () => species.reduce((acc, curr) => {
+const animalListWithName = (options) => species.reduce((acc, curr) => {
   if (!acc[curr.location]) {
     acc[curr.location] = [];
   }
-  const animalName = curr.residents.map((element) => element.name);
+  const animalName = arrayNomes(curr.name, options);
   const obj = {};
   obj[curr.name] = animalName;
   acc[curr.location].push(obj);
   return acc;
-}, animalRegionList);
-
-// console.log(animalListWithName());
+}, {});
+// console.log(animalListWithName({ sex: 'female' }));
 function getAnimalMap(options) {
   // seu código aqui
-  if (!options) {
+  if (options === undefined || options.includeNames === undefined) {
     return animalRegionList();
   }
-  if (!options.includeNames && options.sex) return animalRegionList();
-  return animalListWithName();
+
+  const listaComparam = animalListWithName(options);
+  return listaComparam;
 }
 
-console.log(getAnimalMap({ sex: 'female' }));
+console.log(getAnimalMap({ sorted: true }));
 module.exports = getAnimalMap;
