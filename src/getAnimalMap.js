704ed098faf6b3749d74c12cfc39ec7409909784
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-02 01:39:38 -0300

    requisito 14 teste 1 2 3 passou

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,10 +2,6 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-// const animalRegionList = () => {
-
-// }
-
 const animalRegionList = () => species.reduce((acc, curr) => {
   if (!acc[curr.location]) {
     acc[curr.location] = [];
@@ -14,12 +10,27 @@ const animalRegionList = () => species.reduce((acc, curr) => {
   return acc;
 }, {});
 
-console.log(animalRegionList());
+// console.log(animalRegionList());
+const animalListWithName = () => species.reduce((acc, curr) => {
+  if (!acc[curr.location]) {
+    acc[curr.location] = [];
+  }
+  const animalName = curr.residents.map((element) => element.name);
+  const obj = {};
+  obj[curr.name] = animalName;
+  acc[curr.location].push(obj);
+  return acc;
+}, animalRegionList);
+
+// console.log(animalListWithName());
 function getAnimalMap(options) {
   // seu código aqui
   if (!options) {
     return animalRegionList();
   }
+  if (!options.includeNames && options.sex) return animalRegionList();
+  return animalListWithName();
 }
 
+console.log(getAnimalMap({ sex: 'female' }));
 module.exports = getAnimalMap;
